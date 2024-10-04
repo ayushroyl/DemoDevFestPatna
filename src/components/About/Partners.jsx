d03bb8eb60fd72b985e6013a3b1d52a586724392
mdalkama <mdalkamadad@gmail.com>
2024-10-05 00:26:15 +0530

    partner responsive edit

--- src/components/About/Partners.jsx
+++ src/components/About/Partners.jsx
@@ -27,22 +27,22 @@ export default function Partners(){
 function PartnersCard(props) {
 
   return (
-      <div className="ml-4 mr-4 mt-3 mb-3 p-[3px] box-border shadow-[5px, 5px 5px] shadow-black h-[400px] w-[310px] aspect-video bg-[#F9AA00] border-2 border-[black] rounded-lg flex flex-col justify-between	items-start	" 
+      <div className="ml-4 mr-4 mt-3 mb-3 p-[3px] box-border shadow-[5px, 5px 5px] shadow-black sm:h-[380px] sm:w-[310px] h-[350px] w-[270px] aspect-video bg-[#F9AA00] border-2 border-[black] rounded-lg flex flex-col justify-between	items-start	" 
         style = {{
             boxShadow: "4px 4px 0px 0px black",
         }} >
-          <div className="w-[300px] h-[300px] bg-white border-2 border-[black] rounded-lg flex justify-center items-center	">
+          <div className="sm:w-[300px] sm:h-[300px] w-[260px] h-[260px] bg-white border-2 border-[black] rounded-lg flex justify-center items-center	">
               <img 
               src={props.logo} 
               alt="" 
               className="w-[300px] style:"
               />
           </div>
-          <div className="w-[300px] h-[80px]">
-              <ul className="h-[100%] w-[100%] flex justify-evenly items-center" >
-                <li className="h-[60px] w-[60px] bg-white p-1 rounded-full" ><a  href=""><img className='text-white' src={WebsiteLogo} alt="hey" /></a></li>
-                <li className="h-[60px] w-[60px] bg-white p-1 rounded-full"  ><a href=""><img src={LinkedinLogo} alt="hey" /></a></li>
-                <li className="h-[60px] w-[60px] bg-white p-1 rounded-full"  ><a href=""><img src={TwitterLogo} alt="hey" /></a></li>
+          <div className="sm:w-[300px] sm:h-[80px] w-[260px]">
+              <ul className="h-[80px] w-[100%] flex justify-evenly items-center" >
+                <li className="sm:h-[60px] sm:w-[60px] h-[45px] w-[45px] bg-white p-1 rounded-full" ><a  href=""><img className='text-white' src={WebsiteLogo} alt="hey" /></a></li>
+                <li className="sm:h-[60px] sm:w-[60px] h-[45px] w-[45px] bg-white p-1 rounded-full"  ><a href=""><img src={LinkedinLogo} alt="hey" /></a></li>
+                <li className="sm:h-[60px] sm:w-[60px] h-[45px] w-[45px] bg-white p-1 rounded-full"  ><a href=""><img src={TwitterLogo} alt="hey" /></a></li>
               </ul>
           </div>
       </div>
