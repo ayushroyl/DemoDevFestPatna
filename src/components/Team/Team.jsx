15fddcc85521d02cb31f239c60a5f280ea749f78
mdalkama <mdalkamadad@gmail.com>
2024-10-07 00:42:43 +0530

    team color change

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -18,32 +18,32 @@ import KhushbooGupta from '../../assets/img/team/Khushboo.jpg'
 
 export default function Team() {
   return (
-    <div id="team" className=" w-screen bg-[red] pt-12 bg-white flex items-center flex-col">
+    <div id="team" className=" w-screen  pt-12 bg-white flex items-center flex-col">
         <h1 className="text-black text-center mt-3 mb-5 text-5xl font-bold">Meet Out Team</h1>
         <div className="w-screen pl-8[20%]] pt-[20px] pb-[20px] flex flex-wrap justify-center ">
-            <TeamCard img={AnuragVerma} name="Anurag Verma" role="Organizer" linkedInUrl="https://www.linkedin.com/in/anuragver/" twitterUrl="https://twitter.com/anuragceg`"/>
-            <TeamCard img={BarkhaAgarwal} name="Barkha Agarwal" role="Co-Organizer" linkedInUrl="" twitterUrl=""/>
+            <TeamCard img={AnuragVerma} name="Anurag Verma" role="Organizer" linkedInUrl="https://www.linkedin.com/in/anuragver/" twitterUrl="https://twitter.com/anuragceg`" color="#4C86F9"/>
+            <TeamCard img={BarkhaAgarwal} name="Barkha Agarwal" role="Co-Organizer" linkedInUrl="" twitterUrl="" color="#4C86F9"/>
         </div>
         <h1 className="text-black text-center mt-3 mb-5 text-5xl font-bold">Tech Team</h1>
         <div className="w-screen pl-8[20%]] pt-[20px] pb-[20px] flex flex-wrap justify-center ">
-            <TeamCard img={AbhimanyuYadav} name="Abhimanyu Yadav" role="Technical Lead" linkedInUrl="" twitterUrl=""/>
-            <TeamCard img={BobbyKumar} name="Bobby Kumar" role="Technical Co-Lead" linkedInUrl="" twitterUrl=""/>
-            <TeamCard img={VishhwajeetKumar} name="Vishwajeet Kumar" role="Tech Team" linkedInUrl="" twitterUrl=""/>
+            <TeamCard img={AbhimanyuYadav} name="Abhimanyu Yadav" role="Technical Lead" linkedInUrl="" twitterUrl="" color="#49A84C"/>
+            <TeamCard img={BobbyKumar} name="Bobby Kumar" role="Technical Co-Lead" linkedInUrl="" twitterUrl="" color="#49A84C"/>
+            <TeamCard img={VishhwajeetKumar} name="Vishwajeet Kumar" role="Tech Team" linkedInUrl="" twitterUrl="" color="#49A84C"/>
         </div>
         <h1 className="text-black text-center mt-3 mb-5 text-5xl font-bold">Operation Team</h1>
         <div className="w-screen pl-8[20%]] pt-[20px] pb-[20px] flex flex-wrap justify-center ">
-            <TeamCard img={HarshKapoor} name="Harsh Kapoor" role="Operation Lead" linkedInUrl="" twitterUrl=""/>
-            <TeamCard img={AmikarAnanya} name="Amikar Ananya" role="Operation Co-Lead" linkedInUrl="" twitterUrl=""/>
-            <TeamCard img={TanyaGupta} name="Tanya Gupta" role="Operation Team" linkedInUrl="" twitterUrl=""/>
-            <TeamCard img={MohammadZaidKhan} name="Mohammad Zaid Khan" role="Operation Team" linkedInUrl="" twitterUrl=""/>
-            <TeamCard img={AshishKumar} name="Ashish Kumar" role="Operation Team" linkedInUrl="" twitterUrl=""/>
+            <TeamCard img={HarshKapoor} name="Harsh Kapoor" role="Operation Lead" linkedInUrl="" twitterUrl="" color="#F6BA04"/>
+            <TeamCard img={AmikarAnanya} name="Amikar Ananya" role="Operation Co-Lead" linkedInUrl="" twitterUrl="" color="#F6BA04"/>
+            <TeamCard img={TanyaGupta} name="Tanya Gupta" role="Operation Team" linkedInUrl="" twitterUrl="" color="#F6BA04"/>
+            <TeamCard img={MohammadZaidKhan} name="Mohammad Zaid Khan" role="Operation Team" linkedInUrl="" twitterUrl="" color="#F6BA04"/>
+            <TeamCard img={AshishKumar} name="Ashish Kumar" role="Operation Team" linkedInUrl="" twitterUrl="" color="#F6BA04"/>
         </div>
         <h1 className="text-black text-center mt-3 mb-5 text-5xl font-bold">Design Team</h1>
         <div className="w-screen pl-8[20%]] pt-[20px] pb-[20px] flex flex-wrap justify-center ">
-            <TeamCard img={AsifEkhlaque} name="AsifEkhlaque" role="Design Lead" linkedInUrl="" twitterUrl=""/>
-            <TeamCard img={Sakshi} name="Sakhshi" role="Design Team" linkedInUrl="" twitterUrl=""/>
-            <TeamCard img={NishantKumar} name="NishantKumar" role="Design Team" linkedInUrl="" twitterUrl=""/>
-            <TeamCard img={KhushbooGupta} name="Khushboo Gupta" role="Design Team" linkedInUrl="" twitterUrl=""/>
+            <TeamCard img={AsifEkhlaque} name="AsifEkhlaque" role="Design Lead" linkedInUrl="" twitterUrl="" color="#E1432E"/>
+            <TeamCard img={Sakshi} name="Sakhshi" role="Design Team" linkedInUrl="" twitterUrl=""color="#E1432E"/>
+            <TeamCard img={NishantKumar} name="NishantKumar" role="Design Team" linkedInUrl="" twitterUrl="" color="#E1432E"/>
+            <TeamCard img={KhushbooGupta} name="Khushboo Gupta" role="Design Team" linkedInUrl="" twitterUrl="" color="#E1432E" />
         </div>
     </div>
   )
@@ -56,14 +56,18 @@ export default function Team() {
 function TeamCard(props) {
     return (
         <div className="ml-4 mr-4 mt-3 mb-3 p-[3px] box-border shadow-[5px, 5px 5px] shadow-black h-[330px] w-[280px] aspect-video bg-[#F2EBDA] border-2 border-[black] rounded-lg flex flex-col justify-between	items-start	" 
-          style = {{
-              boxShadow: "6px 6px 0px 0px #F9AA00",
+
+        style = {{
+              boxShadow: `6px 6px 0px 0px ${props.color}`,
           }} >
             <div className="w-[280px] h-[320px] overflow-hidden flex justify-start items-center	flex-col">
                 <img 
                 src={props.img} 
                 alt="" 
-                className="w-[150px] h-[150px] rounded-full mt-[20px] border-4 border-[#F9AA00]"
+                className="w-[150px] h-[150px] rounded-full mt-[20px] border-4"
+                style ={{
+                    borderColor: `${props.color}`
+                }}
                 />
                 <div className='w-[100%] text-center pt-[10px]pb-[10px]'>
                   <p className='text-black text-2xl font-bold pt-[5px]'>{props.name}</p>
